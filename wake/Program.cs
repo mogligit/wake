72a9addfa17ac7118e01450ca3e0c6ef3627579a
mogligit <60526835+mogligit@users.noreply.github.com>
2021-03-28 23:07:32 +0100

    Add project files.

---
+++ wake/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace wake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a new comment
+            Regex macExpression = new Regex("[0-9|a-f|A-F]{2}:[0-9|a-f|A-F]{2}:[0-9|a-f|A-F]{2}:[0-9|a-f|A-F]{2}:[0-9|a-f|A-F]{2}:[0-9|a-f|A-F]{2}");
+
+            if (args.Length < 2)
+            {
+                PrintUsage();
+                return;
+            }
+            else if (macExpression.Match(args[1]).Value != args[1])
+            {
+                Console.WriteLine("MAC address is not valid!");
+                return;
+            }
+
+            string ip = args[0];
+            string mac = args[1];
+            Console.WriteLine("Address -> {0} | MAC -> {1}", ip, mac);
+
+            byte[] magic = new byte[102];
+            byte[] macBytes = new byte[6];
+            string[] macStrings = mac.Split(":");
+
+            //Converts MAC address into bytes
+            for (int i = 0; i <= 5; i++)
+            {
+                macBytes[i] = Convert.ToByte(macStrings[i], 16);
+            }
+
+            /*
+             * Iterates through array and fills first 6 bytes with FF
+             * The rest is the MAC repeated 16 times
+             */
+            for (int i = 0; i <= 5; i++)
+            {
+                magic[i] = 255;
+                for (int j = 1; j <= 16; j++)
+                {
+                    magic[(6 * j) + i] = macBytes[i];
+                }
+            }
+
+            //Sends
+            if (SendData(ip, magic))
+            {
+                Console.WriteLine("Magic packet sent.");
+            }
+        }
+        static void PrintUsage()
+        {
+            Console.WriteLine("Wake On Lan by mogli");
+            Console.WriteLine("USAGE: wake <address> <mac>");
+        }
+        static bool SendData(string ip, byte[] data)
+        {
+            using (UdpClient client = new UdpClient(9))
+            {
+                try
+                {
+                    client.Connect(ip, 9);
+                    client.Send(data, 102);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR: {0}", e.ToString());
+                    return false;
+                }
+            }
+        }
+    }
+}
